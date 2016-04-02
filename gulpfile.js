4cd175496400beb29260eef4f3383bbc58ba0eec
Kévin <k.sourrue@gmail.com>
2016-04-02 16:34:01 +0200

    Init repository

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+/*******************************************
+Gulp dependencies
+********************************************/
+const gulp = require("gulp"),
+      del = require("del"),
+      less = require("gulp-less"),
+      minifyCss = require("gulp-minify-css"),
+      plumber = require("gulp-plumber");
+
+/***************************************************************************************
+Remove Dist directory
+***************************************************************************************/
+gulp.task("clean", (cb) => {
+    
+    return del(["dist"], cb);
+    
+});
+
+/***************************************************************************************
+Compile less files and compress file into Dist directory
+***************************************************************************************/
+gulp.task("less", () => {
+    
+    return gulp.src("./src/css/src/style.less")
+       .pipe(plumber())
+       .pipe(less())
+       .pipe(gulp.dest("./src/css"))
+       .pipe(minifyCss({
+
+            compatibility: "ie8"
+
+        }))
+        .pipe(gulp.dest("./dist/css"));
+    
+});
+
+/***************************************************************************************
+Copy all resources that are not TypeScript files into Dist directory
+***************************************************************************************/
+gulp.task("resources", () => {
+    
+    return gulp.src(["src/**/*", "!**/*.ts", "!src/{css,css}/**"])
+        .pipe(gulp.dest("dist"))
+    
+});
+
+/***************************************************************************************
+Copy all required libraries into Dist and SRC directory
+***************************************************************************************/
+gulp.task("libs", () => {
+    
+    return gulp.src([
+            "es6-shim/es6-shim.min.js",
+            "systemjs/dist/system-polyfills.js",
+            "angular2/bundles/angular2-polyfills.js",
+            "angular2/es6/dev/src/testing/shims_for_IE.js",
+            "systemjs/dist/system.src.js",
+            "rxjs/bundles/Rx.js",
+            "angular2/bundles/angular2.dev.js",
+            "angular2/bundles/router.dev.js"
+        ], {cwd: "node_modules/**"}) /* Glob required here. */
+        .pipe(gulp.dest("src/lib"))
+        .pipe(gulp.dest( "dist/lib"));
+    
+});
+
+/***************************************************************************************
+Build the project
+***************************************************************************************/
+gulp.task("build", ["resources", "less", "libs"], () => {
+    
+    console.log("Building the project ...");
+    
+});
+
+/*******************************************
+Gulp watch
+********************************************/
+gulp.task("watch", () => {
+    
+    gulp.watch("src/css/src/*.less", ["less"]);
+    gulp.watch("src/**/*", ["resources"]);
+    
+});
+
+gulp.task("default", ["watch"]);
---
+++ src/app/app_cmp.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+
+})
+export class AppComponent {
+
+    constructor(){
+        
+        console.log("ok");
+        
+    }
+    
+}
